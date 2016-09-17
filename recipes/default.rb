36c01551361b854fd8bc14a8d9ca111959e25667
Josh Symonds <veraticus@gmail.com>
2016-09-17 02:39:23 -0500

    Remove duplicated service

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,11 +21,6 @@
 
 include_recipe "nginx::#{node['nginx']['install_method']}"
 
-service 'nginx' do
-  supports :status => true, :restart => true, :reload => true
-  action   :start
-end
-
 node['nginx']['default']['modules'].each do |ngx_module|
   include_recipe "nginx::#{ngx_module}"
 end
