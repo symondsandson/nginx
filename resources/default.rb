f4b847ef7e6281803ff7149c039ffbd848bf8f46
Josh Symonds <veraticus@gmail.com>
2016-09-19 17:52:05 -0500

    Use the correct name variable

--- resources/default.rb
+++ resources/default.rb
@@ -8,7 +8,7 @@ property :timing, kind_of: Symbol, default: :delayed
 property :variables, kind_of: Hash, default: {}
 
 action :enable do
-  template "#{node['nginx']['dir']}/sites-available/#{name}" do
+  template "#{node['nginx']['dir']}/sites-available/#{new_resource.name}" do
     owner node['nginx']['user']
     group node['nginx']['user']
     source new_resource.source
@@ -20,19 +20,19 @@ action :enable do
     command "#{node['nginx']['script_dir']}/nxensite #{new_resource.name}"
     notifies :reload, 'service[nginx]', new_resource.timing
     not_if do
-      ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{name}") ||
-      ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/000-#{name}")
+      ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}") ||
+      ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/000-#{new_resource.name}")
     end
   end
 end
 
 action :disable do
   execute "nxdissite #{name}" do
-    command "#{node['nginx']['script_dir']}/nxdissite #{name}"
+    command "#{node['nginx']['script_dir']}/nxdissite #{new_resource.name}"
     notifies :reload, 'service[nginx]', new_resource.timing
     only_if do
-      ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{name}") ||
-      ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/000-#{name}")
+      ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}") ||
+      ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/000-#{new_resource.name}")
     end
   end
 end
